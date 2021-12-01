f4c0fd65ab748e6a453acae6962347352f7ac638
anyuru <davidderrickanyuru@gmail.com>
2021-12-01 17:59:52 +0300

    Refactored schema

--- backend/models/question.js
+++ backend/models/question.js
@@ -1,74 +1,106 @@
 const mongoose = require('mongoose')
+
 const questionSchema = mongoose.Schema({
     test: {
-        testTitle: {
-            type: String,
-            required: true,
-        },
-        testTitle: {
-            type: String,
-            required: true,
-        },
-        allowedStudents: []
-    },
-    question: {
-        unique: 1,
         createdAt: {
-            type: Date, 
+            type: Date,
             default: Date.now()
         },
-        question: {
+        author: {
             type: String,
-            required: 1
+            required: true
         },
-        choice: {
+        title: {
+            type: String,
+            required: true,
+        },
+        allowedStudents: [],
+        questions: {
             type: Array,
-            required: 1,
-        },
-        answer: [],
-        typeOfAnswer: { type: String },
-        author: String,
-        timeLimit: { 
-            type: Number, 
-            required: 1
-        },
-        category: { 
-            type: String,
-            required: 1
-        },
-        isRequired: Boolean,
-        platform: String,
-        score: Number,
-        expiry: {
-            type: Date,
-            required: 1,
-        },
-        answers : {
-            potentialAnswers: []
+            number: {
+                type: Number,
+                required: true,
+                unique: true
+            },
+            elementType: { type: String },
+            createdAt: {
+                type: Date,
+                default: Date.now()
+            },
+            label: {
+                type: String,
+                required: 1
+            },
+            choices: [],
+            answerForChoices: [],
+            potentialAnswersForTextAreaInput: [],
+            radioQuestionAnswer: {
+                type: String
+            },
+            author: String,
+            timeLimit: {
+                type: Number,
+            },
+            category: {
+                type: String,
+                required: 1
+            },
+            isRequired: Boolean,
+            platform: String,
+            score: Number,
+            expireAt: {
+                type: Date,
+            },
+            responses: {
+                type: Array,
+                createdAt: {
+                    type: Date,
+                    default: Date.now,
+                    required: 1
+                },
+                studentId: {
+                    type: String,
+                    required: 1
+                },
+                status: {
+                    type: Boolean
+                },
+                duration: {
+                    type: Number,
+                    required: 1,
+                },
+                score: Number,
+                reviewer: String,
+                remarks: String,
+                rating: Number
+            }
         }
-    }, 
-    responses : {
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        student_id: {
-            type: String,
-            required: 1
-        },
-        status: {
-            type: Boolean
-        },
-        duration: {
-            type: Number,
-            required: 1,
-        },
-        score:  Number,
-        reviewer: String,
-        remarks: String,
-        Rating: Number
     }
 })
 
+question.statics.getTests = (author = null) => {
+    return author !== null ? question.find({ author: author }) : question.find()
+}
+question.statics.getTest = (testId) => {
+    return question.findOne({ _id: testId })
+}
+
+question.statics.update = (data) => {
+    return question.findOneAndUpdate({ _id: data._id }, data)
+}
+
+question.statics.delete = (testId) => {
+    return question.findOneAndDelete({ _id: testId })
+}
+
+question.statics.getQuestions = (testId) => {
+    return question.find({ _id: testId })
+}
+
+question.statics.getQuestion = (testId, questionId) => {
+    return question.findOne({ _id: testId, questions: { $elemMatch: { number: questionId } } })
+}
+
 const question = mongoose.model('question', questionSchema)
 module.exports = question
+
