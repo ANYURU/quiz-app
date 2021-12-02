d2ec8a4dfde72d85e7bfc294e7de953f0e8c2723
anyuru <davidderrickanyuru@gmail.com>
2021-12-02 15:54:51 +0300

    Exam route implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const modelUser = require('../models/user')
-const routerUser = expressRouter()
+const routerUser = Router()
+
+routerUser.get('/', (req, res) => {
+    res.send('All users will be returned')
+})
 
 routerUser.get('/users/username/:username', async (req, res) => {
     const { username } = req.params;
@@ -26,5 +30,85 @@ routerUser.get('/users/username/:username', async (req, res) => {
 
     }
 })
+
+routerUser.post('/login', async (req, res) => {
+    res.json({'message': 'underdevelopment'})
+})
+
+router.post('/resgister', async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const user = new modelUser({ username, password })
+        await user.save()
+
+        if(user !== null) return res.json({
+            'result': 'success',
+            'message': 'Register Successful',
+            'user': user
+        })
+    
+        return res.json({
+            'result': 'failure',
+            'message': 'Register Failed'
+        })
+
+    } catch (error) {
+        return res.json({
+            'result': 'failure',
+            'message': 'Register Failed',
+            'description': 'Maybe username is already taken'
+        })
+    }
+}) 
+
+routerUser.put('/', (req, res) => {
+    console.log(req.body)
+    res.send('Update User')
+})
+
+routerUser.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    let user = {
+        'id': 12,
+        'username': 'David',
+        'gender': 'Male',
+        'email': 'davidderrickanyuru@gmail.com'
+    }
+
+    console.log('Old user', user)
+
+    const { username } = req.body
+    
+    if ( user.id === id ) {
+        user.username = username
+        res.json({
+            'result': 'success',
+            'Username': `Username updated to ${user.username}`,
+            'updated_user': user
+        })
+    } else {
+        res.send(`User ${id} not found`)
+    }
+})
+
+routerUser.delete('/:id', (req, res) => {
+    const { id } = req.params
+    let ids = id.split(',');
+    // console.log(typeof ids)
+    let str = ''
+    let deletedCount = 0
+    ids.forEach(id => {
+        deletedCount++
+        str += `${id},`
+    })
+
+    res.json({
+        'result': 'success',
+        'total_items_deleted': deletedCount,
+        'deleted_items': str
+    })
+})
+
 
 module.exports = routerUser
